7b421b4ed1d07ed3d83a7c758001f4e0c36cdaf3
MrBoriska <mail@borislap.ru>
2021-10-16 17:59:35 +0300

    Fixed installation for second seminar software. Updated tutorial for first seminar.

--- install_ros2_dashing.sh
+++ install_ros2_dashing.sh
@@ -26,12 +26,13 @@ sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -
 sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null'
 
 echo "[Install ROS 2 packages]"
-sudo apt update && sudo apt install -y ros-$name_ros_version-desktop
+sudo apt update
+sudo apt install -y ros-$name_ros_version-desktop
 sudo apt remove -y gazebo11 libgazebo11-dev
 sudo apt install -y gazebo9 libgazebo9-dev
 sudo apt install -y ros-$name_ros_version-gazebo-ros-pkgs \
                     ros-$name_ros_version-joint-state-publisher \
-                    ros-$name_ros_version-joint-state-publisher \
+                    ros-$name_ros_version-xacro \
                     ros-$name_ros_version-rqt-robot-steering \
                     ros-$name_ros_version-cartographer \
                     ros-$name_ros_version-cartographer-ros \
@@ -39,7 +40,8 @@ sudo apt install -y ros-$name_ros_version-gazebo-ros-pkgs \
                     ros-$name_ros_version-nav2-bringup \
                     ros-$name_ros_version-dynamixel-sdk \
                     ros-$name_ros_version-turtlebot3-msgs \
-                    ros-$name_ros_version-turtlebot3
+                    ros-$name_ros_version-turtlebot3 \
+                    ros-$name_ros_version-ament-cmake*
                     
 
 echo "[Environment setup]"
@@ -52,20 +54,23 @@ rosdep update
 echo "[Make the colcon workspace and test colcon build]"
 #mkdir -p $HOME/$name_colcon_workspace/src
 cd $HOME/$name_colcon_workspace
+rosdep install -i --from-path src --rosdistro dashing -y
 colcon build --symlink-install
 
-echo "[Set the ROS evironment]"
-sh -c "echo \"alias nb='nano ~/.bashrc'\" >> ~/.bashrc"
-sh -c "echo \"alias sb='source ~/.bashrc'\" >> ~/.bashrc"
-sh -c "echo \"alias gs='git status'\" >> ~/.bashrc"
-sh -c "echo \"alias gp='git pull'\" >> ~/.bashrc"
+if [ -z "$1" ]; then
+    echo "[Set the ROS evironment]"
+    sh -c "echo \"alias nb='nano ~/.bashrc'\" >> ~/.bashrc"
+    sh -c "echo \"alias sb='source ~/.bashrc'\" >> ~/.bashrc"
+    sh -c "echo \"alias gs='git status'\" >> ~/.bashrc"
+    sh -c "echo \"alias gp='git pull'\" >> ~/.bashrc"
 
-sh -c "echo \"alias cw='cd ~/$name_colcon_workspace'\" >> ~/.bashrc"
-sh -c "echo \"alias cs='cd ~/$name_colcon_workspace/src'\" >> ~/.bashrc"
-sh -c "echo \"alias cb='cd ~/$name_colcon_workspace && colcon build --symlink-install && source ~/.bashrc'\" >> ~/.bashrc"
+    sh -c "echo \"alias cw='cd ~/$name_colcon_workspace'\" >> ~/.bashrc"
+    sh -c "echo \"alias cs='cd ~/$name_colcon_workspace/src'\" >> ~/.bashrc"
+    sh -c "echo \"alias cb='cd ~/$name_colcon_workspace && colcon build --symlink-install && source ~/.bashrc'\" >> ~/.bashrc"
 
-sh -c "echo \"source /opt/ros/$name_ros_version/setup.bash\" >> ~/.bashrc"
-sh -c "echo \"source ~/$name_colcon_workspace/install/local_setup.bash\" >> ~/.bashrc"
+    sh -c "echo \"source /opt/ros/$name_ros_version/setup.bash\" >> ~/.bashrc"
+    sh -c "echo \"source ~/$name_colcon_workspace/install/local_setup.bash\" >> ~/.bashrc"
+fi
 
 source $HOME/.bashrc
 
