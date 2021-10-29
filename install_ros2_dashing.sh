072e1bf83a2f95c8118efa8deadfaa048c4197b9
Boris Lapin <mail@borislap.ru>
2021-10-29 16:04:19 +0300

    Updated for seminar 2

--- install_ros2_dashing.sh
+++ install_ros2_dashing.sh
@@ -21,7 +21,9 @@ sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 
 echo "[Setup Sources]"
-sudo rm -rf /var/lib/apt/lists/* && sudo apt update && sudo apt install -y curl gnupg2 lsb-release
+sudo rm -rf /var/lib/apt/lists/*
+sudo apt update 
+sudo apt install -y curl gnupg2 lsb-release
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
 sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null'
 
