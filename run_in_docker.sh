d21ea8c0279a1fb7b3440edae3ba38907877d8ab
Boris Lapin <mail@borislap.ru>
2022-11-30 18:20:44 +0300

    Added seminar 3 (beta)

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 xhost +local:docker
-docker run -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix: -v /home/mrboriska/ros2_seminars_ws:/ros2_seminars_ws -it  osrf/ros:humble-desktop
+docker run -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix: -v /home/mrboriska/ros2_seminars_ws:/ros2_seminars_ws -it --name ros2  osrf/ros:humble-desktop
